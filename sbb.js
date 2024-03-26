6a668d2b1037112ea3e26dfbf885d7477f8fbb5d
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-03-26 22:24:00 +0500

    Add files via upload

---
+++ sbb.js
@@ -0,0 +1,5 @@
+//Example of subtraction.
+var noOfFriends = 4;
+var noOfTotalFriends = 14;
+var noOfAbsentFriends = (noOfTotalFriends - noOfFriends);
+console.log(noOfAbsentFriends);
---
+++ sbb.ts
@@ -0,0 +1,6 @@
+//Example of subtraction.
+let noOfFriends : number = 4
+let noOfTotalFriends : number = 14
+let noOfAbsentFriends : number = (noOfTotalFriends - noOfFriends);
+
+console.log(noOfAbsentFriends);
