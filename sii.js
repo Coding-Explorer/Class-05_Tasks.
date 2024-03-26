e7c8a334a2ff01cf86b722438bf9dad60cdc8397
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-03-26 22:29:49 +0500

    Add files via upload

---
+++ sii.js
@@ -0,0 +1,5 @@
+//Example of Combining operators.
+var result = 8 + 2 * 6;
+result++;
+console.log(result);
+// Answer will be 21
---
+++ sii.ts
@@ -0,0 +1,6 @@
+//Example of Combining operators.
+
+let result = 8 + 2 * 6;
+result++
+console.log(result)
+// Answer will be 21 or something else?
