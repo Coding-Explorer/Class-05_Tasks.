51514d5bd8141a78925f1c94a62b2510618c3f83
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-03-26 22:29:05 +0500

    Add files via upload

---
+++ shh.js
@@ -0,0 +1,5 @@
+//Example of Modulus (%).
+var mango = 7;
+var bags = 4;
+var remainder = mango % bags;
+console.log(remainder); // 3
---
+++ shh.ts
@@ -0,0 +1,6 @@
+//Example of Modulus (%).
+let mango = 7
+let bags = 4
+let remainder = mango % bags;
+
+console.log(remainder) // 3
