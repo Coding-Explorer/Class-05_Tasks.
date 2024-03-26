f87b1453e63abb5af36bd6522e5f47f099aaa33b
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-03-26 22:22:51 +0500

    Add files via upload

---
+++ saa.js
@@ -0,0 +1,5 @@
+//Example of addition.
+var membersOfFaculty = 6;
+var membersOfManagement = 12;
+var totalNoOfFaculty = membersOfFaculty + membersOfManagement;
+console.log(totalNoOfFaculty);
---
+++ saa.ts
@@ -0,0 +1,7 @@
+
+//Example of addition.
+let membersOfFaculty : number = 6;
+let membersOfManagement : number = 12;
+let totalNoOfFaculty : number = membersOfFaculty + membersOfManagement;
+
+console.log(totalNoOfFaculty);
