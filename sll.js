542d5d3eef947e95d93b38cf81504c53e09bb81f
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-03-26 22:31:55 +0500

    Add files via upload

---
+++ sll.js
@@ -0,0 +1,7 @@
+//Example of Logical operators.
+var c = 7;
+var d = 4;
+var logicalAnd = (c > 0) && (d > 0); //true statement.
+var logicalOr = (c < 0) || (d > 0); //true statement.
+var logicalNot = !(c > 0); //false statement.
+console.log(logicalNot);
---
+++ sll.ts
@@ -0,0 +1,9 @@
+//Example of Logical operators.
+
+let c = 7
+let d = 4
+let logicalAnd = (c > 0) && (d > 0); //true statement.
+let logicalOr = (c < 0) || (d > 0); //true statement.
+let logicalNot = !(c > 0); //false statement.
+
+console.log(logicalNot)
