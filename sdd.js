100bbe024a4d7543065eeade0f1083646f9096fd
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-03-26 22:25:39 +0500

    Add files via upload

---
+++ sdd.js
@@ -0,0 +1,5 @@
+//Example of Division.
+var num1 = 36;
+var num2 = 5;
+var resultantNum = (num1 / num2);
+console.log(resultantNum);
---
+++ sdd.ts
@@ -0,0 +1,6 @@
+//Example of Division.
+let num1 : number = 36
+let num2 : number = 5
+let resultantNum : number = (num1 / num2);
+
+console.log(resultantNum);
