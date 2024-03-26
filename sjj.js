43b5ed8afe92de0f5359987486c76c3d3f662f12
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-03-26 22:30:29 +0500

    Add files via upload

---
+++ sjj.js
@@ -0,0 +1,5 @@
+//Example of Assignment operators (=).
+var quantity = 12;
+quantity += 6;
+// equivalent to quantity = quantity + 6 , quantity is now 18
+console.log(quantity);
---
+++ sjj.ts
@@ -0,0 +1,7 @@
+//Example of Assignment operators (=).
+
+let quantity = 12;
+quantity += 6;
+// equivalent to quantity = quantity + 6 , quantity is now 18
+
+console.log(quantity)
