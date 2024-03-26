9b14f11b31032a22d99023d23fc81e2c3fa141d8
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-03-26 22:34:35 +0500

    This is the complete work that have practiced.

---
+++ task05.js
@@ -0,0 +1,12 @@
+//usage of pre & post increment(++index & index++) and pre & post decrement(--index & index--).
+//let a:number=5;
+//let b:number=2;
+//let c:number;
+//c=++a + a++ +--b +b-- +a+ b++ +b;
+//console.log(c);
+// 6+ 7 + 1 + 0 + 5 + 1 + 2
+//Example of addition.
+var membersOfFaculty = 6;
+var membersOfManagement = 12;
+var totalNoOfFaculty = membersOfFaculty + membersOfManagement;
+console.log(totalNoOfFaculty);
---
+++ task05.ts
@@ -0,0 +1,7 @@
+//usage of pre & post increment(++index & index++) and pre & post decrement(--index & index--).
+let a:number=5;
+let b:number=2;
+let c:number;
+c=++a + a++ +--b +b-- +a+ b++ +b;
+console.log(c);
+// 6+ 7 + 1 + 0 + 5 + 1 + 2
