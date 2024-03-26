79a1a730ff42dd20f5a0806b2aa2d53da5bdacbf
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-03-26 22:31:10 +0500

    Add files via upload

---
+++ skk.js
@@ -0,0 +1,8 @@
+//Example of Comparison operators.
+var a = 8;
+var b = 6;
+var isEqual = (a == b); //false statement.
+var isNotEqual = (a != b); //true statement.
+var isGreaterThan = (a > b); //true statement.
+var isLessThan = (a < b); //false statement.
+console.log(a == b);
---
+++ skk.ts
@@ -0,0 +1,10 @@
+//Example of Comparison operators.
+
+let a = 8
+let b = 6
+let isEqual = (a == b); //false statement.
+let isNotEqual = (a != b); //true statement.
+let isGreaterThan = (a > b); //true statement.
+let isLessThan = (a < b); //false statement.
+
+console.log(a == b)
