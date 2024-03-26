a57f779ae5cedb44f2c63851d14968187b6d57c2
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-03-26 22:24:52 +0500

    Add files via upload

---
+++ scc.js
@@ -0,0 +1,5 @@
+//Example of Multiplication.
+var a = 6;
+var b = 9;
+var c = (a * b);
+console.log(c);
---
+++ scc.ts
@@ -0,0 +1,6 @@
+//Example of Multiplication.
+let a : number = 6
+let b : number = 9
+let c : number = (a * b);
+
+console.log(c);
