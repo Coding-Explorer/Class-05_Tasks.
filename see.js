27eb7b1ed84aa6860b430f051292e31ad03ee033
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-03-26 22:26:29 +0500

    Add files via upload

---
+++ see.js
@@ -0,0 +1,5 @@
+//Combined example of Addition, subtraction, multiplication and division.
+var num1 = 2;
+var num2 = 5;
+var num3 = ((num1 + num2)(num1 - num2)(num1 * num2)(num1 / num2));
+console.log(num3);
---
+++ see.ts
@@ -0,0 +1,6 @@
+//Combined example of Addition, subtraction, multiplication and division.
+let num1 = 2
+let num2 = 5
+let num3 = ((num1 + num2)(num1 - num2 )(num1 * num2)(num1 / num2 ))
+
+console.log(num3);
