b946a2b72de9228b3c41e07e2846562a6e6716fe
Coding-Explorer <160587282+Coding-Explorer@users.noreply.github.com>
2024-03-26 22:28:23 +0500

    Add files via upload

---
+++ sgg.js
@@ -0,0 +1,5 @@
+//Example of Exponentiation (**).
+var rows = 10;
+var mango = 10;
+var power = Math.pow(rows, 2);
+console.log(power); // 
---
+++ sgg.ts
@@ -0,0 +1,6 @@
+//Example of Exponentiation (**).
+let rows = 10
+let mango = 10
+let power = rows**2;
+
+console.log(power) // 100 
